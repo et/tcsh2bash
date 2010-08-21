8cdc4b191e70c6978de3cbaf211931ec2e111d67
et <eric.l.m.thomas@gmail.com>
2010-08-21 02:18:30 -0400

    Moved parentheses --> bracket conversion down to the boolean level since they're easier to deal with there

--- spec/parsers/conditional_parser_spec.rb
+++ spec/parsers/conditional_parser_spec.rb
@@ -6,48 +6,32 @@ module Tcsh2Bash
       @parser = ConditionalParser.new
     end
 
-    describe "bool_expr" do
-      it "should parse $a > 1" do
-        parse("$a > 1").should_not be_nil
-      end
-
-      it "should parse 1 < $a" do
-        parse("1 < $a").should_not be_nil
-      end
-    end
-
-=begin
     describe "one line if statement" do
-      it "should parse this one line if statement" do
-        parse("if (boolean) alias fire water").should_not be_nil
-      end
-
       it "should convert it a one line if statement to bash" do
 
         output = <<EOF
-if [ boolean ]
+if [ $foo == "foo" ]
 then
   echo "Hello world!"
 fi
 EOF
         output.strip!
 
-        convert_to_bash('if (boolean) echo "Hello world!"').should == output
+        convert_to_bash('if ($foo == "foo") echo "Hello world!"').should == output
       end
 
       it "should convert assignments when possible" do
         output = <<EOF
-if [ boolean ]
+if [ $foo -gt 2 ]
 then
   alias foo=bar
 fi
 EOF
         output.strip!
 
-        convert_to_bash('if (boolean) alias foo bar').should == output
+        convert_to_bash('if ($foo > 2) alias foo bar').should == output
       end
     end
-=end
 
   end
 end
