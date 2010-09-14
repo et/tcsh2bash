5f3ec4781216b92043e2327e321434f4a2c422a3
eric <eric.thomas@faa.gov>
2010-09-14 14:42:36 -0400

    Adjusted the comment grammar

--- spec/parsers/tcsh_parser_spec.rb
+++ spec/parsers/tcsh_parser_spec.rb
@@ -27,7 +27,28 @@ EOF
     end
 
     describe 'comment' do
-      specify { parse("# some comment").should_not be_nil }
+      single_comment = "#single comment"
+      specify { convert_to_bash(single_comment).should == single_comment }
+
+      multi_comment = <<EOF.strip
+#######################
+# Multi line comment
+# more comments
+#######################
+EOF
+      specify { convert_to_bash(multi_comment).should == multi_comment }
+
+      it 'should handle a command following a command' do
+
+        multi_comment_with_command_after = multi_comment + "\n"
+        multi_comment_with_command_after += <<EOF.strip
+setenv EDITOR vim
+EOF
+        convert_to_bash(multi_comment_with_command_after).should == multi_comment + "\nexport EDITOR=vim"
+       
+      end
+
+      #puts convert_to_bash(multi_comment_with_command_after)
     end
 
   end
